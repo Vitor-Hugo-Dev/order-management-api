2d0850bf93cad462411f661984fb015037a8fea3
vitor Hugo <vitorhugo.dev1997@gmail.com>
2025-11-10 22:51:56 -0300

    feat: configs jwt e spring security

---
+++ src/main/java/com/order/management/demo/config/AuditorAwareImpl.java
@@ -0,0 +1,24 @@
+package com.order.management.demo.config;
+
+import com.order.management.demo.model.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class AuditorAwareImpl implements AuditorAware<UUID> {
+
+    @Override
+    public Optional<UUID> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")) {
+            return Optional.empty();
+        }
+
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Optional.of(userPrincipal.getId());
+    }
+}
