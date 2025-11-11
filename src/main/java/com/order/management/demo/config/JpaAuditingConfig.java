558670a044b296676e9a40890446fbc8027adcf1
vitor Hugo <vitorhugo.dev1997@gmail.com>
2025-11-10 22:51:10 -0300

    feat: configs jwt e spring security

---
+++ src/main/java/com/order/management/demo/config/JpaAuditingConfig.java
@@ -0,0 +1,18 @@
+package com.order.management.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.UUID;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class JpaAuditingConfig {
+
+    @Bean
+    public AuditorAware<UUID> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+}
